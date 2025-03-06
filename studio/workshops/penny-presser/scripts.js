c5e350b2770aa225615817948eb6c6b405f35265
stras328 <tacocatcupcake@gmail.com>
2025-03-06 10:00:58 -0500

    added classmate options

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,7 +1,8 @@
 // get the press button
 var pressButton = document.querySelector ('button[name="press"');
-var pennySelect = document.querySelector ('select[name="penny"]')
-var pennyImage = document.querySelector ('.penny')
+var pennySelect = document.querySelector ('select[name="penny"]');
+var studentSelect = document.querySelector ('select[name="student"]');
+var pennyImage = document.querySelector ('.penny');
 
 // log the elements
 console.log(pressButton);
@@ -10,11 +11,16 @@ console.log(pennySelect.value);
 // add event listener to press button
 pressButton.addEventListener ('click', () => {
 
+    // log the student value
+    console.log(studentSelect.value);
+
     // log the penny value
     console.log(pennySelect.value);
 
     // set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
+        //  pennyImage.src = 'img/' + pennySelect.value;
+    pennyImage.src = 'https://' + studentSelect.value + '.interaction.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
+
 
     // log the penny image
     console.log(pennyImage);
